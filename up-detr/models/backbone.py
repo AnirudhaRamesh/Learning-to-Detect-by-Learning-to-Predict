e7cc375bc014848830f56a7929ec2a0f3f1b9721
Nikhil Angad Bakshi <bakshienator77@gmail.com>
2022-05-10 02:23:40 +0000

    the rest of the fusion changes for updetr, using unified mask based on swav features

--- up-detr/models/backbone.py
+++ up-detr/models/backbone.py
@@ -77,36 +77,54 @@ class BackboneBase(nn.Module):
             self.body_ours = IntermediateLayerGetter(backbone_ours, return_layers=return_layers)
             self.num_channels = 2*num_channels
         else:
-            self.backbone_ours = None
+            self.body_ours = None
             self.num_channels = num_channels
 
     def forward(self, tensor_list):
         """supports both NestedTensor and torch.Tensor
         """
-        if isinstance(tensor_list, NestedTensor):
-            xs = self.body(tensor_list.tensors)
-            out: Dict[str, NestedTensor] = {}
-            for name, x in xs.items():
-                m = tensor_list.mask
-                assert m is not None
-                mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
-                out[name] = NestedTensor(x, mask)
-        else:
-            out = self.body(tensor_list)
-        if self.backbone_ours is not None:
+
+        if self.body_ours is not None:
             if isinstance(tensor_list, NestedTensor):
-                xs = self.body_ours(tensor_list.tensors)
+                xs = self.body(tensor_list.tensors)
+                xs_ours = self.body_ours(tensor_list.tensors)
                 out_ours: Dict[str, NestedTensor] = {}
+                out: Dict[str, NestedTensor] = {}
+                for ((name, x), (name_ours, x_ours))  in zip(xs.items(), xs_ours.items()):
+                    m = tensor_list.mask
+                    # print("MASK shape is: ", m.shape)
+                    # print("X shape is: ", x.shape)
+                    assert m is not None
+                    mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
+                    # print("MASK shape is: ", mask.shape)
+                    mask_ours = F.interpolate(m[None].float(), size=x_ours.shape[-2:]).to(torch.bool)[0]
+                    out[name] = NestedTensor(torch.cat([x, x_ours], dim=1), mask)
+
+            else:
+                out = self.body(tensor_list)
+                out_ours = self.body_ours(tensor_list)
+                for key in out.keys():
+                    feat_ours, mask_ours = out_ours[key].decompose()
+                    feat, mask = out[key].decompose()
+                    # print("MASK shape is: ", mask.shape)
+                    # print("X shape is: ", feat.shape)
+                    out[key] = NestedTensor(torch.cat([feat, feat_ours], dim=1), mask)
+
+            # print()
+            # print("expecting that out shape: ", out.shape, " and out_ours.shape: ", out_ours.shape, " are the same and should be concatenated alon the channels")
+            # out = torch.cat([out, out_ours], dim=1)
+            # print("concatted out is: ", out.shape)
+        else:
+            if isinstance(tensor_list, NestedTensor):
+                xs = self.body(tensor_list.tensors)
+                out: Dict[str, NestedTensor] = {}
                 for name, x in xs.items():
                     m = tensor_list.mask
                     assert m is not None
                     mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
-                    out_ours[name] = NestedTensor(x, mask)
+                    out[name] = NestedTensor(x, mask)
             else:
-                out_ours = self.body_ours(tensor_list)
-            print("expecting that out shape: ", out.shape, " and out_ours.shape: ", out_ours.shape, " are the same and should be concatenated alon the channels")
-            out = torch.cat([out, out_ours], dim=1)
-            print("concatted out is: ", out.shape)
+                out = self.body(tensor_list)
         return out
 
 
@@ -137,11 +155,12 @@ class Backbone(BackboneBase):
             print("LOADING OUR SAVED MODELLLLLL!!!!!!\n\n\n\n", custom_model_path)
             if custom_model_path == "":
                 print("Who do you who do you think you are no custome model path but specifiying fuse?")
-                raise Error
+                raise Exception
             checkpoint = torch.load(custom_model_path,map_location="cpu")
             state_dict = {k.replace("module.", ""): v for k, v in checkpoint.items() if "fc" not in k}
             # print(state_dict)
             backbone_ours.load_state_dict(state_dict, strict=False)
+            num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
 
             super().__init__(backbone, train_backbone, num_channels, return_interm_layers, backbone_ours)
         else:
@@ -190,3 +209,6 @@ def build_backbone(args):
     model = Joiner(backbone, position_embedding)
     model.num_channels = backbone.num_channels
     return model
+
+
+#simple shoulder kaizen
